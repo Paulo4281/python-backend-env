23b75428fcc5232b57d4a71812bc1b954594fb01
Paulo Oliveira <138153192+Paulo4281@users.noreply.github.com>
2024-02-16 18:59:41 -0300

    feat: Implementing find() method in book_repository

--- src/modules/book/repositories/book_repository.py
+++ src/modules/book/repositories/book_repository.py
@@ -3,6 +3,7 @@ from src.database.database_config import session
 from src.modules.book.dtos.book_dto import BookDTO, BookResponseDTO
 from uuid import uuid4
 from datetime import datetime
+from typing import List
 
 class BookRepository:
     @staticmethod
@@ -22,6 +23,22 @@ class BookRepository:
                 session.add(book)
 
             return book.to_dict()
+        except:
+            session.rollback()
+        finally:
+            session.close()
+
+    @staticmethod
+    def find() -> List[BookResponseDTO]:
+        try:
+            with session.begin():
+                books = session.query(Book)
+
+                books_list = []
+
+                for book in books:
+                    books_list.append(book.to_dict())
+                return books_list
         except:
             session.rollback()
         finally:
