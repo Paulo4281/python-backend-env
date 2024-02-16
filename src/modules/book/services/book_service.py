1ca3b28bea507241d6d0aa94425e77798bd9c57b
Paulo Oliveira <138153192+Paulo4281@users.noreply.github.com>
2024-02-16 18:53:20 -0300

    feat: Implementing find() method in book_service

--- src/modules/book/services/book_service.py
+++ src/modules/book/services/book_service.py
@@ -1,7 +1,12 @@
 from src.modules.book.dtos.book_dto import BookDTO, BookResponseDTO
 from src.modules.book.repositories.book_repository import BookRepository
+from typing import List
 
 class BookService:
     @staticmethod
     def save(data: BookDTO) -> BookResponseDTO:
         return BookRepository.save(data)
+
+    @staticmethod
+    def find() -> List[BookResponseDTO]:
+        return BookRepository.find()
