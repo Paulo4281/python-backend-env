e5b8b51a82fd14e4755b5a16b628bb1126e6859e
Paulo Wellington <paulo@yaayoo.com.br>
2024-02-22 20:02:22 -0300

    feat: Validating database connection in try/except block

--- src/database/database_config.py
+++ src/database/database_config.py
@@ -6,7 +6,12 @@ import os
 load_dotenv()
 
 engine = create_engine(f"{os.getenv("ENGINE")}://{os.getenv("DATABASE_USER")}:@{os.getenv("DATABASE_HOST")}:{os.getenv("DATABASE_PORT")}/{os.getenv("DATABASE_NAME")}", echo=False)
-connection = engine.connect()
+
+try:
+    connection = engine.connect()
+    print("Database connected!")
+except Exception as e:
+    print(str(e))
 
 Session = sessionmaker(bind=engine)
 session = Session()
